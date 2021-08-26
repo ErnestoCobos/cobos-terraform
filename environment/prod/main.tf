5fe0e66b8be892187a519d04d218fa9134dcf8ae
Ernesto Cobos <ernesto.cobos@narvar.com>
2021-08-26 00:47:52 -0500

    fix: removed records variable

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -5,5 +5,5 @@ resource "aws_route53_zone" "primary" {
 module "cobos_dns" {
   source = "../../modules/route53"
   hosted_zone = var.hosted_zone
-  records_a = var.records_a
+  records_a = { "luna.cobos.io": "23.94.207.169" }
 }
--- environment/prod/variables.tf
+++ environment/prod/variables.tf
@@ -2,7 +2,3 @@ variable "hosted_zone" {
   type = string
   description = "It's the top-level domain"
 }
-
-variable "records_a" {
-  description = "List of records"
-}
