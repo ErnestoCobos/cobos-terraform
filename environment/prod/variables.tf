7eb7ca827a93bcc64b3acb4026c6670f82c90901
Ernesto Cobos <ernesto.cobos@narvar.com>
2021-08-26 00:35:17 -0500

    fix: removed records variable

--- environment/prod/variables.tf
+++ environment/prod/variables.tf
@@ -2,13 +2,3 @@ variable "hosted_zone" {
   type = string
   description = "It's the top-level domain"
 }
-
-variable "records" {
-  type = list(object({
-    type = string
-    name = string
-    ttl = string
-    records = list(string)
-  }))
-  description = "List of records"
-}
